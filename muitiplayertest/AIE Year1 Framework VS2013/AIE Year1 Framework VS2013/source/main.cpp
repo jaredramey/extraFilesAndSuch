2ad057c27a4c15de8a69dc909646be7a2ef5b5e5
Jared Ramey <rameyjared@gmail.com>
2014-09-03 12:34:22 -0700

    Space Invaders are Back
    After the scare space invaders should be back

---
+++ muitiplayertest/AIE Year1 Framework VS2013/AIE Year1 Framework VS2013/source/main.cpp
@@ -0,0 +1,73 @@
+﻿#include "AIE.h"
+#include <iostream>
+#include <windows.h>
+#include <fstream>
+
+int main( int argc, char* argv[] )
+{	
+    Initialise(1024, 700, false, "My Awesome Game");
+    
+    SetBackgroundColour(SColour(0, 0, 0, 255));
+
+    int myTextureHandle = CreateSprite("./images/crate_sideup.png", 64, 64, true);
+    float xPos = 100;
+    float yPos = 100;
+
+    //Game Loop
+    do
+    {
+        MoveSprite(myTextureHandle, xPos, yPos);
+        DrawSprite(myTextureHandle);
+
+
+		//KeyPress way1 GetAsyncKeyState(VK_[key]);
+		///*
+		if (GetAsyncKeyState(VK_LEFT))
+		{
+			if (xPos > 0)
+			{
+				xPos--;
+			}
+		}
+
+		else if (GetAsyncKeyState(VK_RIGHT))
+		{
+			if (xPos < 1024)
+			{
+				xPos++;
+			}
+		}
+
+		else if (GetAsyncKeyState(VK_DOWN))
+		{
+			if (yPos > 0)
+			{
+				yPos--;
+			}
+		}
+
+		else if (GetAsyncKeyState(VK_UP))
+		{
+			if (yPos < 700)
+			{
+				yPos++;
+			}
+		}
+		//*/
+
+		/* Play around with this movement type [AIE header movement function]
+		if (IsKeyDown(glfw key values))
+		{
+			yPos++;
+		}
+		*/
+
+
+        ClearScreen();
+
+    } while(!FrameworkUpdate());
+
+    Shutdown();
+
+    return 0;
+}
