0c675f0f246000b3401274310453a272f945d320
Jared Ramey <rameyjared@gmail.com>
2015-01-06 12:28:26 -0800

    Tutorial 2 done
    I now have the power of a very colorful triangle! Fear the rainbow!

--- IntroToOpenGL/Tutorial_Attempt2/Tutorial_Attempt2/Main.cpp
+++ IntroToOpenGL/Tutorial_Attempt2/Tutorial_Attempt2/Main.cpp
@@ -8,6 +8,7 @@
 //Function Prototypes
 GLuint CreateShader(GLenum a_eShaderType, const char *a_strShaderFile);
 GLuint CreateProgram(const char *a_vertex, const char *a_frag);
+float* getOrtho(float left, float right, float bottom, float top, float a_fNear, float a_fFar);
 
 
 int main()
@@ -16,14 +17,6 @@ int main()
 	//Initialise GLFW
 	if (!glfwInit())
 	{
-		return -1;
-	}
-
-	//Initialize Glew
-	if (glewInit() != GLEW_OK)
-	{
-		// OpenGL didn't start-up! shutdown GLFW and return an error code
-		glfwTerminate();
 		return -1;
 	}
 
@@ -36,13 +29,65 @@ int main()
 		return -1;
 	}
 
+
 	//make the window's context current
 	glfwMakeContextCurrent(window);
+
+	//Initialize Glew
+	if (glewInit() != GLEW_OK)
+	{
+		// OpenGL didn't start-up! shutdown GLFW and return an error code
+		glfwTerminate();
+		return -1;
+	}
+
+
+	const float vertexPositions[] =
+	{
+		1024/2.0, 720/2.0 + 200.0f, 0.0f, 1.0f,
+		1024/2.0 - 100.0f, 720/2.0 - 200.0f, 0.0f, 1.0f,
+		1024/2.0 + 100.0f, 720/2.0 - 200.0f, 0.0f, 1.0f,
+	};
+
+	const float VertexColours[] =
+	{
+		1.0f, 0.0f, 0.0f, 1.0f,
+		0.0f, 1.0f, 0.0f, 1.0f,
+		0.0f, 0.0f, 1.0f, 1.0f,
+	};
+
+	//crate shader program
+	GLuint uiProgramFlat = CreateProgram("VertexShader.glsl", "FlatFragmentShader.glsl");
+
+	//find the position of the matrix var in the shader so we can send info there later
+	GLuint MatrixIDFlat = glGetUniformLocation(uiProgramFlat, "MVP");
+
+	//set up the mapping of the screen to pixel co-ordinates. Try changing values and see what happens
+	float *orthographicProjection = getOrtho(0, 1080, 0, 720, 0, 180); 
 
 	//loop until the user closes the window
 	while (!glfwWindowShouldClose(window))
 	{
 		//draw code goes here
+		glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		//enable shaders
+		glUseProgram(uiProgramFlat);
+
+		//send orthographic projection info to shader
+		glUniformMatrix4fv(MatrixIDFlat, 1, GL_FALSE, orthographicProjection);
+
+		//enable the vertex array state, since we're sending in an array of verticies
+		glEnableVertexAttribArray(0);
+		glEnableVertexAttribArray(1);
+
+		//Specify where our vertex array is, how many compnents each vertex has, the data of each component and whether the data is normalised or not
+		glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, vertexPositions);
+		glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, VertexColours);
+
+		//draw to screen
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		//swap front and back buffers
 		glfwSwapBuffers(window);
@@ -147,4 +192,25 @@ GLuint CreateProgram(const char *a_vertex, const char *a_frag)
 	}
 
 	return uiProgram;
+}
+
+float* getOrtho(float left, float right, float bottom, float top, float a_fNear, float a_fFar)
+{
+	//to correspond with mat4 in the shader
+	//ideally this function would be part of your matrix class
+	//however I wasn't willing to write your matrix class for you just to show you this
+	//so here we are in array format!
+	//add this to your matrix class as a challenge if you like!
+	float* toReturn = new float[12];
+	toReturn[0] = 2.0 / (right - left);;
+	toReturn[1] = toReturn[2] = toReturn[3] = toReturn[4] = 0;
+	toReturn[5] = 2.0 / (top - bottom);
+	toReturn[6] = toReturn[7] = toReturn[8] = toReturn[9] = 0;
+	toReturn[10] = 2.0 / (a_fFar - a_fNear);
+	toReturn[11] = 0;
+	toReturn[12] = -1 * ((right + left) / (right - left));
+	toReturn[13] = -1 * ((top + bottom) / (top - bottom));
+	toReturn[14] = -1 * ((a_fFar + a_fNear) / (a_fFar - a_fNear));
+	toReturn[15] = 1;
+	return toReturn;
 }
--- IntroToOpenGL/Tutorial_Attempt2/Tutorial_Attempt2/VertexShader.glsl
+++ IntroToOpenGL/Tutorial_Attempt2/Tutorial_Attempt2/VertexShader.glsl
@@ -2,7 +2,7 @@
 
 layout(location = 0) in vec4 position;
 layout(location = 1) in vec4 colour;
-layout(location = 2) in vec4 vertexUV;
+layout(location = 2) in vec2 vertexUV;
 
 uniform mat4 MVP;
 
@@ -12,9 +12,9 @@ out vec2 UV;
 void main()
 {
 	vertColour = colour;
-	UV = vertexUX;
+	UV = vertexUV;
 
 	vec4 scaledPosition = MVP * position;
 
-	gl_scaledPosition;
+	gl_Position = scaledPosition;
 }
