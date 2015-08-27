5d8e52a628bd39b61d69d5d63ab8e57a5b7d43d4
Jared Ramey <rameyjared@gmail.com>
2015-08-27 10:41:50 -0700

    Problem 3 finished

--- EulerProblems/Euler_Problem3/Euler_Problem3/Main.cpp
+++ EulerProblems/Euler_Problem3/Euler_Problem3/Main.cpp
@@ -2,10 +2,77 @@
 
 using namespace std;
 
+long long int FindLargestPrime(long long int num);
+bool is_perfect_square(long long int n);
+bool is_prime(unsigned long long n);
+
 int main()
 {
-	int num = 600851475143;
+	long long int num = 600851475143;
+	long long int largestPrime;
+
+	largestPrime = FindLargestPrime(num);
+
+	cout << largestPrime << endl;
+
 
 	system("PAUSE");
 	return 0;
+}
+
+long long int FindLargestPrime(long long int num)
+{
+	long long int result = 0;
+	long long int i = round(sqrt(num));
+	
+	while (i <= num / 2 && result == 0)
+	{
+		if (i % 2 != 0)
+		{
+			if (num%i == 0)
+			{
+				if (is_prime(i) != true)
+				{
+					result = i;
+				}
+				else
+				{
+					i--;
+				}
+			}
+			else
+			{
+				i--;
+			}
+		}
+
+		else
+		{
+			i--;
+		}
+	}
+
+	return result;
+}
+
+bool is_perfect_square(long long int n)
+{
+	if (n < 0)
+		return false;
+
+	long long int root(round(sqrt(n)));
+	return n == root * root;
+}
+
+bool is_prime(unsigned long long n)
+{
+	for (long long int i = 3; i < n; i += 2)
+	{
+		if (n%i == 0)
+		{
+			return true;
+		}
+	}
+
+	return false;
 }
