c5b6f5e557126157e55d403b371dc16c819c1fba
Jared Ramey <rameyjared@gmail.com>
2015-08-27 12:16:05 -0700

    Finished problem 6 and started 7

--- EulerProblems/Euler_Problem6/Euler_Problem6/Main.cpp
+++ EulerProblems/Euler_Problem6/Euler_Problem6/Main.cpp
@@ -4,6 +4,26 @@ using namespace std;
 
 int main()
 {
+	int sumOfSquare = 0;
+	int squareOfSum = 0;
+	int total = 0;
+	int result;
+
+	for (int i = 1; i <= 100; i++)
+	{
+		sumOfSquare += i*i;
+	}
+
+	for (int j = 1; j <= 100; j++)
+	{
+		total += j;
+	}
+
+	squareOfSum = total * total;
+
+	result = squareOfSum - sumOfSquare;
+
+	cout << result << endl;
 	
 	system("PAUSE");
 	return 0;
